f8cc4019e7321c709dcda39bd72245cab7068375
Dakshin D <dakshindeva1999@gmail.com>
2022-07-25 12:37:43 +0530

    Initial spring boot application files

---
+++ src/main/java/com/bank/accountmanagementsystem/AccountmanagementsystemApplication.java
@@ -0,0 +1,14 @@
+package com.bank.accountmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmanagementsystemApplication.class, args);
+		System.out.println("Starting application");
+	}
+
+}
---
+++ src/test/java/com/bank/accountmanagementsystem/AccountmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.accountmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
