e66b62ae1155fa4814cf5edf7958bb6f5cfd968d
Dontag <nishad.patil@mu-zero.io>
2022-07-26 10:00:16 +0530

    added comments in customercontroller

--- src/main/java/com/bank/accountmanagementsystem/Controllers/CustomerController.java
+++ src/main/java/com/bank/accountmanagementsystem/Controllers/CustomerController.java
@@ -11,23 +11,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.accountmanagementsystem.Services.CustomerService;
+import com.bank.accountmanagementsystem.constants.SystemConstants;
 
-
+/**
+ * CustomerController (define the meaning of the class)
+ * 
+ * @author Nishad
+ *
+ */
 @RestController
 public class CustomerController {
-	
+	/**
+	 * customerService
+	 */
 	@Autowired
 	private CustomerService customerService;
-	
-	@GetMapping("api/customer/get-accounts/{id}")
-	public ResponseEntity<Object> getAccountsByCustId(@PathVariable int id){
-		Set<HashMap<String,String>> resultSet = customerService.getAccountDetailsByCustomerId(id);
-		HashMap<String,Object> result = new HashMap<String, Object>();
-		if(resultSet.isEmpty()) {
-			result.put("message", "accounts not found for "+Integer.toString(id));
-			return new ResponseEntity<>(result,HttpStatus.NOT_FOUND);
+
+	/**
+	 * getAccountsByCustId (this is responsible to give the customer ....)
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@GetMapping(SystemConstants.GET_ACCOUNTS_BY_CUSTOMERID)
+	public ResponseEntity<Object> getAccountsByCustId(@PathVariable int id) {
+		Set<HashMap<String, String>> resultSet = customerService.getAccountDetailsByCustomerId(id);
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		if (resultSet.isEmpty()) {
+			result.put("message", "accounts not found for " + Integer.toString(id));
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
 		}
 		result.put("accounts", resultSet);
-		return new ResponseEntity<>(result,HttpStatus.OK);
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/bank/accountmanagementsystem/constants/SystemConstants.java
@@ -0,0 +1,5 @@
+package com.bank.accountmanagementsystem.constants;
+
+public interface SystemConstants {
+  public static final String GET_ACCOUNTS_BY_CUSTOMERID = "/accounts/customer/{customerId}";
+}
